9bd43498501054e0785268dabd1e3d1b5ec3d622
Cat Hanbit <futurely08@naver.com>
2022-11-25 14:06:13 +0900

    오버라이딩

--- 9.클래스/5.overriding_classvariable.py
+++ 9.클래스/5.overriding_classvariable.py
@@ -6,6 +6,7 @@
             드래곤 클래스에 인스턴스 속성을 추가하기
             부모 클래스에 클래스 변수 추가하기
 '''
+import random
 
 # 부모 클래스
 class Monster:
@@ -18,3 +19,54 @@ class Monster:
 
     def move(self):
         print(f"[{self.name}] 지상에서 이동하기")
+
+# 자식 클래스
+class Wolf(Monster):
+    pass
+
+class Shark(Monster):
+    def move(self):       # 메서드 오버라이딩
+        print(f"[{self.name}] 헤엄치기")
+
+class Dragon(Monster):
+    # 생성자 오버라이딩
+    def __init__(self, name, health, attack):
+        super().__init__(name, health, attack)
+        self.skills = ("불뿜기", "꼬리치기", "날개치기")
+    def move(self):
+        print(f"[{self.name}] 날기")
+
+    def skill(self):
+        print(f"[{self.name}] 스킬 사용 {self.skills[random.randint(0,2)]}")
+
+wolf = Wolf("울프", 1500, 200)
+wolf.move()
+print(wolf.max_num)
+
+shark = Shark("샤크", 3000, 400)
+shark.move()
+print(shark.max_num)
+
+dragon = Dragon("드래곤", 8000, 800)
+dragon.move()
+dragon.skill()
+print(dragon.max_num)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
